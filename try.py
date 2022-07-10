fa7de0aa8eeaae806efe8ccdf8f2f38cc442b949
Mr. Robot <mrrobottest@gmail.com>
2022-07-11 01:49:12 +0300

    foor loops

--- try.py
+++ try.py
@@ -1,4 +1,14 @@
 from datetime import date
 today = date.today()
 
+list_of_number = [2, 5, 6, 3, 4, 9, 2]
+new_list = []
+for number in list_of_number:
+    inc_number = number + 1
+    if inc_number < 6:
+        new_list.append(number)
+print(new_list)
+
+sample_dict = dict()
+
 
